7493aa3031993521217dccc048e11933fba08651
Juan Lassalle <juanlassalle23@hotmail.com>
2025-05-03 01:01:59 -0300

    primer proyecto

---
+++ Proyecto-01/assets/js/main.js
@@ -0,0 +1,12 @@
+let nombre = "Juan";
+let apellido = "Lassalle";
+let edad = 44;
+let pais = "Argentina";
+let conocimiento = ["HTML", "CSS", "JavaScript"];
+
+console.log(`Mi nombre es ${nombre} ${apellido}, tengo ${edad} años y soy de ${pais}`)
+
+edad = 43;
+conocimiento.push("React","Node.js")
+
+console.log(`Tengo estos conocimientos: ${conocimiento[3]} y ${conocimiento[4]}`)
